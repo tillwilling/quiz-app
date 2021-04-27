867715dd5da1580295e2c822a5a1d51d9c1e4efb
Till Willing <tillwilling@gmail.com>
2021-04-27 16:54:10 +0200

    totally lost

---
+++ src/index.js
@@ -0,0 +1,53 @@
+const buttonHome = document.querySelector('.button-home')
+const buttonBookmarks = document.querySelector('.button-bookmarks')
+const buttonCreate = document.querySelector('.button-create')
+const buttonSettings = document.querySelector('.button-settings')
+
+const homePage = document.querySelector('.page-home')
+const bookmarksPage = document.querySelector('.page-bookmarks')
+const createPage = document.querySelector('.page-create')
+const settingsPage = document.querySelector('.page-settings')
+
+buttonHome.addEventListener('click', () => {
+  homePage.classList.remove('hidden')
+  bookmarksPage.classList.add('hidden')
+  createPage.classList.add('hidden')
+  settingsPage.classList.add('hidden')
+  buttonHome.classList.add('active')
+  buttonBookmarks.classList.remove('active')
+  buttonCreate.classList.remove('active')
+  buttonSettings.classList.remove('active')
+})
+
+buttonBookmarks.addEventListener('click', () => {
+  homePage.classList.add('hidden')
+  bookmarksPage.classList.remove('hidden')
+  createPage.classList.add('hidden')
+  settingsPage.classList.add('hidden')
+  buttonHome.classList.remove('active')
+  buttonBookmarks.classList.add('active')
+  buttonCreate.classList.remove('active')
+  buttonSettings.classList.remove('active')
+})
+
+buttonCreate.addEventListener('click', () => {
+  homePage.classList.add('hidden')
+  bookmarksPage.classList.add('hidden')
+  createPage.classList.remove('hidden')
+  settingsPage.classList.add('hidden')
+  buttonHome.classList.remove('active')
+  buttonBookmarks.classList.remove('active')
+  buttonCreate.classList.add('active')
+  buttonSettings.classList.remove('active')
+})
+
+buttonSettings.addEventListener('click', () => {
+  homePage.classList.add('hidden')
+  bookmarksPage.classList.add('hidden')
+  createPage.classList.add('hidden')
+  settingsPage.classList.remove('hidden')
+  buttonHome.classList.remove('active')
+  buttonBookmarks.classList.remove('active')
+  buttonCreate.classList.remove('active')
+  buttonSettings.classList.add('active')
+})
